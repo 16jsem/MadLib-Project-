1fc7a8e932b8848b7b451b3d39e5c6cd459e47fb
Coder <coder@mainlab19.local>
2016-01-08 10:57:56 -0800

    .

--- CoupevilleLibs/main.swift
+++ CoupevilleLibs/main.swift
@@ -9,4 +9,47 @@
 import Foundation
 
 print("Hello, World!")
+//  CoupevilleLibs
+
+import Foundation
+print("CoupevilleLibs")
+print("Hello, World!")
+
+print("Madlib Menu")
+
+func menuChoice(choice: String)
+print("Madlib Menu")
+
+
+
+
+
+// madlib #1
+
+
+
+func input() -> String {
+    var keyboard = NSFileHandle.fileHandleWithStandardInput()
+    var inputData = keyboard.availableData
+    var strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
+    
+    return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+}
+
+
+print("My company")
+print("Name of company")
+var A = input()
+print("Is developing")
+print("A defined offering")
+var B = input()
+print("to help")
+print("A defined audience")
+var C = input()
+print("Solve a problem")
+var D = input()
+print("With")
+print("Secret suace")
+var E = input()
+
 
