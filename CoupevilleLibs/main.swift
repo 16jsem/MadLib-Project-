966e4a414422def384375ea101b9b5a6189dc20d
Coder <coder@mainlab24.local>
2016-01-12 11:16:24 -0800

    bjh

--- CoupevilleLibs/main.swift
+++ CoupevilleLibs/main.swift
@@ -35,33 +35,102 @@ while keepPlaying == true {
 
 
 
-func input() -> String {
-    var keyboard = NSFileHandle.fileHandleWithStandardInput()
-    var inputData = keyboard.availableData
-    var strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
-    
-    return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
-}
-
-
-print("My company")
-print("Name of company")
-var A = input()
-print("Is developing")
-print("A defined offering")
-var B = input()
-print("to help")
-print("A defined audience")
-var C = input()
-print("Solve a problem")
-var D = input()
-print("With")
-print("Secret sauce")
-var E = input()
-
-
-
-
+        
+        
+        print("Hello, World!")
+        
+        
+        
+        
+        
+        
+        
+        func input() -> String {
+            var keyboard = NSFileHandle.fileHandleWithStandardInput()
+            var inputData = keyboard.availableData
+            var strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
+            
+            return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+        }
+        
+        print("My company")
+        print("Name of company")
+        var A = input()
+        print("Is developing")
+        print("A defined offering")
+        var B = input()
+        print("to help")
+        print("A defined audience")
+        var C = input()
+        print("Solve a problem")
+        var D = input()
+        print("With")
+        print("Secret suace")
+        var E = input()
+        
+        
+        
+        
+        
+        // print
+        
+        
+        
+        
+        
+        
+        func input1() -> String {
+            var keyboard = NSFileHandle.fileHandleWithStandardInput()
+            var inputData = keyboard.availableData
+            var strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
+            
+            return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+        }
+        
+        
+        
+        
+        
+        
+        
+        print("Female celeb")
+        var madlib5 = input()
+        print("'s first husband, the")
+        print("adjective")
+        var madlib4 = input()
+        print("that is")
+        print("male celeb")
+        var madlib3 = input()
+        print("was spotted")
+        var madlib2 = input()
+        print("adverb")
+        print("canoodling with")
+        var madlib1 = input()
+        print("female celeb")
+        print("who he met filming")
+        var madlib0 = input ()
+        print("name of movie")
+        print("in")
+        var madlib6 = input()
+        print("place")
+        print("sadly, the romance ended after")
+        var madlib7 = input()
+        print("small number")
+        print("months, when")
+        var madlib8 = input()
+        print("same male celeb")
+        print("past tense verd")
+        var madlib9 = input ()
+        
+        
+        
+        
+        
+        
+        //print
+        
+        
+        
 
 
 // madlib #2
